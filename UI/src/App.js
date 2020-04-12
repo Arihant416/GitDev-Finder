a036e37cb1a30eb7bc8d24a2ca2790ad16f0f150
Arihant416 <arihantjain416@gmail.com>
2020-04-12 10:30:04 +0530

    Import Navbar and UserItem

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const name = 'Arihant Jain';
-
+  render () {
     return (
-      <div className='App'>
-        <h1>Hello {name}</h1>
+      <div className="App">
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
