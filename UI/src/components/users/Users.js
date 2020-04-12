b4a2d10bda4eb729022661b319b4aff3a03537e1
Arihant416 <arihantjain416@gmail.com>
2020-04-12 12:31:40 +0530

    Inline styled and mapped list of  Developers

--- UI/src/components/users/Users.js
+++ UI/src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, {Component} from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'Arihant416',
-        avatar_url: 'https://avatar0.githubusercontent.com/u/1/?v=4',
-        html_url: 'https://github.com/Arihant416',
-      },
-      {
-        id: '1',
-        login: 'Arihant416',
-        avatar_url: 'https://avatar0.githubusercontent.com/u/1/?v=4',
-        html_url: 'https://github.com/Arihant416',
-      },
-      {
-        id: '1',
-        login: 'Arihant416',
-        avatar_url: 'https://avatar0.githubusercontent.com/u/1/?v=4',
-        html_url: 'https://github.com/Arihant416',
-      },
-    ],
-  };
   render () {
     return (
       <div style={userStyle}>
-        {this.state.users.map (user => <UserItem key={user.id} user={user} />)}
+        {this.props.users.map (user => <UserItem key={user.id} user={user} />)}
       </div>
     );
   }
