3732c94e33fbdac098037a0f38677bbdb2dbcd65
Arihant416 <arihantjain416@gmail.com>
2020-04-12 21:21:14 +0530

    Add About Component

---
+++ UI/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About GitDev Finder</h1>
+      <p>
+        This is a simple web app for users to search for a developer's Github profile
+        {' '}
+      </p>
+      <p>Version 1.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
